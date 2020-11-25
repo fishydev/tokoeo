68fca0b8b67df45ddbb5efc163ed8ec488cdb13f
fishydev <huvuhuvuhuvu@gmail.com>
2020-11-25 11:32:56 +0700

    homepage progress

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCk_3tY9o4Z9gDHHunttL3jqpCLcELswKE",
+  authDomain: "tokoeo-25905.firebaseapp.com",
+  databaseURL: "https://tokoeo-25905.firebaseio.com",
+  projectId: "tokoeo-25905",
+  storageBucket: "tokoeo-25905.appspot.com",
+  messagingSenderId: "85773324130",
+  appId: "1:85773324130:web:939aecaed8e4104514e63b"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,133 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-app-bar color="primary" absolute>
+      <p class="h3">TokoEO</p>
+      <v-spacer></v-spacer>
+      <v-btn class="mx-4" depressed @click="loginCard()">Login</v-btn>
+      <v-btn depressed @click="registerCard()">Register</v-btn>
+    </v-app-bar>
+    <v-img src="https://picsum.photos/1920/1080" height="80vh"></v-img>
+    <v-overlay :value="loginOverlay" :dark="false">
+      <v-card class="pa-0">
+        <v-row class="pa-0 ma-0">
+          <v-col class="pa-8">
+            <v-form align="center">
+              <v-text-field
+                label="Email"
+                required
+                v-model="login.email"
+              ></v-text-field>
+              <v-text-field
+                label="Password"
+                required
+                v-model="login.password"
+              ></v-text-field>
+              <v-btn type="submit">Login</v-btn>
+            </v-form>
+          </v-col>
+          <v-col class="pa-0 ma-0 d-none d-lg-block">
+            <v-img
+              class="rounded-tr rounded-br"
+              src="https://picsum.photos/1920/1080"
+              height="300px"
+              max-width="245px"
+            ></v-img>
+            <v-btn
+              color="white"
+              icon
+              right
+              absolute
+              top
+              text
+              @click="loginCard()"
+            >
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-overlay>
+    <v-overlay :value="registerOverlay" :dark="false">
+      <v-card class="pa-8">
+        <v-form align="center">
+          <v-spacer></v-spacer>
+          <v-btn icon absolute top right text @click="registerCard()">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-text-field
+            label="Email"
+            required
+            v-model="register.email"
+          ></v-text-field>
+          <v-text-field
+            label="Password"
+            required
+            v-model="register.password"
+          ></v-text-field>
+          <v-btn @click="submitRegister">Register</v-btn>
+        </v-form>
+      </v-card>
+    </v-overlay>
+    <v-snackbar class="pr-4" v-model="error.isError" bottom color="red">
+      {{ error.message }}
+      <v-icon class="float-right">mdi-close</v-icon>
+    </v-snackbar>
   </div>
 </template>
 
+<style lang="css">
+@import "../assets/styles.css";
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import firebase from "firebase";
 
 export default {
+  data() {
+    return {
+      loginOverlay: false,
+      registerOverlay: false,
+      login: {
+        email: "",
+        password: ""
+      },
+      register: {
+        email: "",
+        password: ""
+      },
+      error: {
+        isError: false,
+        message: ""
+      }
+    };
+  },
+
+  methods: {
+    loginCard() {
+      this.loginOverlay = !this.loginOverlay;
+    },
+
+    registerCard() {
+      this.registerOverlay = !this.registerOverlay;
+    },
+
+    submitRegister() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(
+          this.register.email,
+          this.register.password
+        )
+        .catch(err => {
+          this.error.message = err.message;
+          this.error.isError = true;
+          console.log(err.message);
+        });
+    }
+  },
   name: "Home",
   components: {
-    HelloWorld
+    //
   }
 };
 </script>
