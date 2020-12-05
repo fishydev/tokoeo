971738f4997b2c195ee96d65d1b5d243d0f695a5
fishydev <huvuhuvuhuvu@gmail.com>
2020-12-05 18:57:13 +0700

    update responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,20 +8,40 @@
             max-height="50px"
             max-width="75px"
             contain
+            @click="toPage('Dashboard')"
           ></v-img>
         </v-col>
-        <v-col cols="6">
+        <v-col sm="3" md="3" class="px-0 mx-0 d-md-none">
           <div class="d-flex justify-center">
-            <v-btn text depressed color="#dddddd" large>Categories</v-btn>
+            <!-- <v-btn text depressed color="#dddddd" large>Browse</v-btn> -->
+          </div>
+        </v-col>
+        <v-col lg="6" sm="3" md="3" class="d-none d-lg-block">
+          <div class="d-flex justify-center">
             <v-btn
               text
               depressed
               color="#dddddd"
               large
-              @click="toPage('explore')"
+              @click="toPage('Categories')"
+              >Categories</v-btn
+            >
+            <v-btn
+              text
+              depressed
+              color="#dddddd"
+              large
+              @click="toPage('Explore')"
               >Explore</v-btn
             >
-            <v-btn text depressed color="#dddddd" large>Vendor</v-btn>
+            <v-btn
+              text
+              depressed
+              color="#dddddd"
+              large
+              @click="toPage('Vendor')"
+              >Vendor</v-btn
+            >
           </div>
         </v-col>
         <v-col cols="3">
@@ -29,7 +49,7 @@
             <v-btn icon text color="#dddddd">
               <v-icon>mdi-cart</v-icon>
             </v-btn>
-            <v-btn icon text color="#dddddd" @click="toPage('dashboard')">
+            <v-btn icon text color="#dddddd" @click="toPage('Dashboard')">
               <v-icon>mdi-account-circle</v-icon>
             </v-btn>
             <v-btn icon text color="#dddddd">
@@ -44,11 +64,18 @@
 
 <script>
 export default {
+  data: () => ({
+    items: [
+      { name: "Explore", value: "Explore" },
+      { name: "Categories", value: "Categories" },
+      { name: "Vendor", value: "Vendor" }
+    ]
+  }),
   name: "Navbar",
   methods: {
     toPage(page) {
-      if (page !== this.$route.name.toLowerCase()) {
-        this.$router.push(`/${page}`);
+      if (page !== this.$route.name) {
+        this.$router.push({ name: page });
       } else {
         this.$forceUpdate();
       }
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="dashboard">
     <Navbar />
-    <v-row class="py-16" justify="center">
-      <v-col cols="4">
+    <v-row class="py-16 px-4" justify="center">
+      <v-col md="10" lg="4" sm="10">
         <div class="user-profile-card">
-          <v-card class="mx-auto" width="400px" height="600px">
+          <v-card class="mx-auto" height="400px">
             <v-card-title>
-              <v-avatar class="mx-auto" size="200">
+              <v-avatar class="mx-auto" size="100">
                 <img src="../assets/avatar.png" alt="avatar" />
               </v-avatar>
             </v-card-title>
             <v-card-subtitle class="mt-4">
               <p class="display-1 text-center">John Doe</p>
             </v-card-subtitle>
+            <v-card-actions>
+              <v-btn
+                class="mx-auto"
+                color="#222831"
+                dark
+                @click="toPage('VendorDashboard')"
+                >Dashboard vendor</v-btn
+              >
+            </v-card-actions>
           </v-card>
         </div>
       </v-col>
-      <v-col cols="6">
+      <v-col md="10" lg="6" sm="10">
         <div class="d-flex flex-column">
           <v-card class="mb-4 pa-4">
             <p class="display-1">Your Events</p>
@@ -29,7 +38,14 @@
             >
               <p color="#dddddd">{{ order.text }}</p>
               <div class="d-flex justify-end">
-                <v-btn class="mx-2" right color="#222831">Detail</v-btn>
+                <v-btn
+                  class="mx-2"
+                  right
+                  color="#222831"
+                  @click="toPage('Event Plan')"
+                  dark
+                  >Detail</v-btn
+                >
                 <v-btn right color="#f05454">Cancel</v-btn>
               </div>
             </v-card>
@@ -41,9 +57,10 @@
                     fab
                     color="#222831"
                     small
+                    dark
                     v-on="on"
                     v-bind="attrs"
-                    @click="toPage('Create Event')"
+                    @click="createEvent()"
                   >
                     <v-icon>mdi-plus</v-icon>
                   </v-btn>
@@ -62,23 +79,99 @@
             >
               <p color="#dddddd">{{ history.text }}</p>
               <div class="d-flex justify-end">
-                <v-btn color="#222831">Detail</v-btn>
+                <v-btn color="#222831" dark>Detail</v-btn>
               </div>
             </v-card>
           </v-card>
         </div>
       </v-col>
     </v-row>
+    <v-overlay :value="createEventOverlay" :dark="false">
+      <v-card width="400px">
+        <v-card-title
+          >Create Event
+          <v-spacer></v-spacer>
+          <v-btn icon @click="createEvent()">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-divider></v-divider>
+        <div class="px-4 pt-4">
+          <v-form ref="formEvent" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="eventName"
+              placeholder="Nama Event"
+              prepend-icon="mdi-rename-box"
+              :rules="required"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="eventAddress"
+              placeholder="Alamat"
+              prepend-icon="mdi-google-maps"
+              :rules="required"
+              required
+            ></v-text-field>
+            <v-menu
+              ref="dateMenu"
+              v-model="dateMenu"
+              :close-on-content-click="false"
+              :return-value.sync="eventDate"
+              transition="scale-transition"
+              content-class="elevation-0"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="eventDate"
+                  :rules="required"
+                  prepend-icon="mdi-calendar"
+                  v-bind="attrs"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker v-model="eventDate" no-title scrollable flat>
+                <v-spacer></v-spacer>
+                <v-btn text color="primary" @click="dateMenu = false"
+                  >Cancel</v-btn
+                >
+                <v-btn
+                  text
+                  color="primary"
+                  @click="$refs.dateMenu.save(eventDate)"
+                  >OK</v-btn
+                >
+              </v-date-picker>
+            </v-menu>
+            <v-row>
+              <v-spacer></v-spacer>
+              <v-btn class="ma-4" color="success" @click="submitEvent()"
+                >Buat Event</v-btn
+              >
+            </v-row>
+          </v-form>
+        </div>
+      </v-card>
+    </v-overlay>
   </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar";
+import firebase from "firebase";
+
+var db = firebase.firestore();
 
 export default {
   data: () => ({
+    valid: true,
     orders: [{ text: "Lorem Ipsum" }, { text: "Dolor Sit Amet" }],
-    histories: [{ text: "Lorem Ipsum" }, { text: "Dolor Sit Amet" }]
+    histories: [{ text: "Lorem Ipsum" }, { text: "Dolor Sit Amet" }],
+    createEventOverlay: false,
+    required: [value => !!value || "Required."],
+    eventName: "",
+    eventAddress: "",
+    eventDate: new Date().toISOString().substr(0, 10),
+    dateMenu: false
   }),
   methods: {
     toPage(page) {
@@ -87,6 +180,28 @@ export default {
       } else {
         this.$forceUpdate();
       }
+    },
+
+    createEvent() {
+      this.createEventOverlay = !this.createEventOverlay;
+    },
+
+    submitEvent() {
+      this.$router.push({ name: "Event Plan" });
+      db.collection("events")
+        .add({
+          eventName: this.eventName,
+          eventAddress: this.eventAddress,
+          eventDate: this.eventDate,
+          planner: localStorage.getItem("user")
+        })
+        .then(res => {
+          console.log("Created event with ID: ", res.id);
+          this.createEvent();
+        })
+        .catch(err => {
+          console.error("Error: ", err);
+        });
     }
   },
   name: "Dashboard",
